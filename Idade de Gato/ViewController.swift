257a8d24158c73d4b86aa422dac04d74c8217527
Viktor Rocha <viktoorrocha@gmail.com>
2017-09-20 23:08:44 -0400

    First code is cat

---
+++ Idade de Gato/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Idade de Gato
+//
+//  Created by Viktor Rocha on 20/09/17.
+//  Copyright © 2017 Viktor Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
